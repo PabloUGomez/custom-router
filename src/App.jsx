8ca9d6075311614c907e16db060818920121a2b7
Pablo Gomez <pablogomez0430@gmail.com>
2024-04-04 16:20:03 -0300

    add navigator, converted to SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+const NAVIGATION_EVENT = "pushstate";
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
+}
 
 function HomePage() {
   return (
     <>
       <h1>Home Page</h1>
       <p>Esta es una pagina de ejemplo para crear un React Router desde cero</p>
-      <a href="/about"> Ir a sobre nosotros</a>
+      <button onClick={()=>navigate('/about')}> Ir a sobre nosotros</button>
     </>
   );
 }
@@ -16,17 +22,32 @@ function AboutPage() {
     <>
       <h1>About Page</h1>
       <div>
-        <img style={{borderRadius: "100%"}} src="https://media.licdn.com/dms/image/D4D03AQHv-ROZTzpOkw/profile-displayphoto-shrink_200_200/0/1700790225530?e=1717632000&v=beta&t=cKaItCEf3shiZCoKlSdRPMqHnKMEDBqkg_ODFAw67WY" alt="Foto de Pablo Gomez" />
-        <p>¡Hola! Me llamo Pablo Gomez y estoy creando un clon de React Router.</p>
+        <img
+          style={{ borderRadius: "100%" }}
+          src="https://media.licdn.com/dms/image/D4D03AQHv-ROZTzpOkw/profile-displayphoto-shrink_200_200/0/1700790225530?e=1717632000&v=beta&t=cKaItCEf3shiZCoKlSdRPMqHnKMEDBqkg_ODFAw67WY"
+          alt="Foto de Pablo Gomez"
+        />
+        <p>
+          ¡Hola! Me llamo Pablo Gomez y estoy creando un clon de React Router.
+        </p>
       </div>
-      <a href="/"> Ir a la Home</a>
+      <button onClick={()=>navigate('/')}> Ir a la Home</button>
     </>
   );
 }
 
 function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
 
-  const [currentPath,setCurrentPath] = useState(window.location.pathname)
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+    };
+  }, []);
 
   return (
     <main>
