3f419960a5a39211b5ec093604fd49a8b4982eca
Pablo Gomez <pablogomez0430@gmail.com>
2024-04-10 21:11:54 -0300

    fix 0.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import {Suspense } from "react";
 
-import { Route } from "./Route.jsx";
+import {Router} from "./Router.jsx";
 import HomePage from "./pages/Home.jsx";
 import AboutPage from "./pages/About.jsx";
 import SearchPage from "./pages/Search.jsx";
 import Page404 from "./pages/404.jsx"
 
+const routes = [
+  { path: "/", Component: HomePage },
+  { path: "/about", Component: AboutPage },
+  { path: "/search/:query", Component: SearchPage },
+];
 
 function App() {
   return (
     <main>
       <Suspense fallback={<h1>Loading...</h1>}>
-          <Route path="/" Component={HomePage} />
-          <Route path="/about" Component={AboutPage} />
-          <Route path="/search/:query" Component={SearchPage} />
-          <Route path="/*" Component={Page404}/>
+          <Router routes={routes} defaultComponent={Page404}/>
       </Suspense>
     </main>
   );
